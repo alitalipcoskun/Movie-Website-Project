b4e3d5a748198a67779aa12144b768fcb97a8000
Ali Talip COŞKUN <ali_talip_18@hotmail.com>
2022-09-12 21:59:12 +0300

    Refactoring Code
    I try to make my code more re-usable and getting help from my course. I created an autocomplete function in order to use again and again in my project.

---
+++ autocomplete.js
@@ -0,0 +1,51 @@
+const createAutoComplete = ({root}) => {
+root.innerHTML = `
+    <label><b>Search For a Movie </b></label>
+    <input class = "input"/>
+    <div class = "dropdown">
+        <div class = "dropdown-menu">
+            <div class ="dropdown-content results"></div>
+        </div>   
+    </div>
+`;
+//I do not have to search for all DOCUMENT. I should find what i want from root
+const input = root.querySelector('input');
+const dropdown = root.querySelector('div .dropdown');
+const resultWrapper = root.querySelector('.results');
+
+const onInput = async (event) => {
+    const movies = await fetchData(event.target.value);
+    if(!movies.length){
+        dropdown.classList.remove('is-active');// this will say if we do not get anything from API, do not open searching results bar.
+        return;//it will end function early than usual.
+    }
+
+    resultWrapper.innerHTML = ""; // That is to make it clear the list for right after the searches.
+    dropdown.classList.add('is-active');
+    for(let movie of movies){
+        const option = document.createElement('a');
+        const imgSrc = movie.Poster === 'N/A' ? '': movie.Poster;//Checking for empty image, rather than getting error message.
+        option.classList.add('dropdown-item');
+        option.innerHTML = `
+            <img src = "${imgSrc}">
+            ${movie.Title}
+        `;
+        option.addEventListener('click', () => {
+            dropdown.classList.remove('is-active');// After selecting the film, searchbar will get removed.
+            input.value  = movie.Title;//Clicked film will be in searchbar's text.
+            onMovieSelect(movie);
+
+        });
+
+        resultWrapper.appendChild(option);
+    }
+    
+};
+input.addEventListener('input', debounce(onInput, 500))
+document.addEventListener('click', event => {
+    console.log(event.target);// it will allow us the element when we click.
+    if(!root.contains(event.target)){//root is the searchbar.
+        dropdown.classList.remove('is-active');//it will help us to close it.
+    }
+}); //It has been added in order to get rid of the filmwrapper while user click different part of page.
+}
--- index.js
+++ index.js
@@ -16,55 +16,15 @@ const fetchData = async (searchTerm) => {
 };
 
 
-const root = document.querySelector('div .autocomplete');
-root.innerHTML = `
-    <label><b>Search For a Movie </b></label>
-    <input class = "input"/>
-    <div class = "dropdown">
-        <div class = "dropdown-menu">
-            <div class ="dropdown-content results"></div>
-        </div>   
-    </div>
-`;
-const input = document.querySelector('input');
-const dropdown = document.querySelector('div .dropdown');
-const resultWrapper = document.querySelector('.results');
-
-const onInput = async (event) => {
-    const movies = await fetchData(event.target.value);
-    if(!movies.length){
-        dropdown.classList.remove('is-active');// this will say if we do not get anything from API, do not open searching results bar.
-        return;//it will end function early than usual.
-    }
-
-    resultWrapper.innerHTML = ""; // That is to make it clear the list for right after the searches.
-    dropdown.classList.add('is-active');
-    for(let movie of movies){
-        const option = document.createElement('a');
-        const imgSrc = movie.Poster === 'N/A' ? '': movie.Poster;//Checking for empty image, rather than getting error message.
-        option.classList.add('dropdown-item');
-        option.innerHTML = `
-            <img src = "${imgSrc}">
-            ${movie.Title}
-        `;
-        option.addEventListener('click', () => {
-            dropdown.classList.remove('is-active');// After selecting the film, searchbar will get removed.
-            input.value  = movie.Title;//Clicked film will be in searchbar's text.
-            onMovieSelect(movie);
-
-        });
-
-        resultWrapper.appendChild(option);
-    }
-    
-};
-input.addEventListener('input', debounce(onInput, 500))
-document.addEventListener('click', event => {
-    console.log(event.target);// it will allow us the element when we click.
-    if(!root.contains(event.target)){//root is the searchbar.
-        dropdown.classList.remove('is-active');//it will help us to close it.
-    }
-}); //It has been added in order to get rid of the filmwrapper while user click different part of page.
+createAutoComplete({
+    root : document.querySelector('div .autocomplete')
+});
+createAutoComplete({
+    root : document.querySelector('div .autocomplete-two')
+});
+createAutoComplete({
+    root : document.querySelector('div .autocomplete-three')
+});
 
 
 const onMovieSelect = async movie  => {
@@ -92,6 +52,26 @@ const movieTemplate = (movieDetail) => {
                     <p>${movieDetail.Plot}</p>
                 </div>
             </div>
+        </article>
+        <article class = "notification is-primary">
+            <p class = "title">${movieDetail.Awards}</p>
+            <p class = "subtitle">Awards</p>
+        </article>
+        <article class = "notification is-primary">
+            <p class = "title">${movieDetail.BoxOffice}</p>
+            <p class = "subtitle">Box Office</p>
+        </article>
+        <article class = "notification is-primary">
+            <p class = "title">${movieDetail.Metascore}</p>
+            <p class = "subtitle">Metascore</p>
+        </article>
+        <article class = "notification is-primary">
+            <p class = "title">${movieDetail.imdbRating}</p>
+            <p class = "subtitle">IMDB Rating</p>
+        </article>
+        <article class = "notification is-primary">
+            <p class = "title">${movieDetail.imdbVotes}</p>
+            <p class = "subtitle">IMDB Votes</p>
         </article>
     `;
 };
